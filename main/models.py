807095f18e5b8169996efcd1e60e6c1815afe717
peradoce <gperinho20@gmail.com>
2017-06-15 10:22:20 +0000

    configuring settings

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 # Create your models here.
+
+class UserImage(models.Model):
+    user = models.ForeignKey(User)
+    date = models.DateField()
+    image = ResizedImageField(size=[500, 300], quality=75, upload_to='users_avatar')
